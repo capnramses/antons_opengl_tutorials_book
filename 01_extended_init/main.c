03113deabbef2060ae30bdffc7fd513e5a0ce267
eljaydub <lee.wasilenko@gmail.com>
2014-08-10 18:32:19 +0200

    Fix bug in _update_fps_counter
    The version of _update_fps_counter currently on github does not work, but the version on the website and in the kindle ebook are fine. I just replaced the broken github code with the code from the website and everything works now, afaik.

--- 01_extended_init/main.c
+++ 01_extended_init/main.c
@@ -151,28 +151,21 @@ void log_gl_params () {
 	gl_log ("-----------------------------\n");
 }
 
-double previous_seconds;
-int frame_count;
-
 /* we will use this function to update the window title with a frame rate */
 void _update_fps_counter (GLFWwindow* window) {
-	double current_seconds;
-	double elapsed_seconds;
-
-	previous_seconds = glfwGetTime ();
-	current_seconds = glfwGetTime ();
-	elapsed_seconds = current_seconds - previous_seconds;
-	if (elapsed_seconds > 0.25) {
-		char tmp[128];
-		double fps = (double)frame_count / elapsed_seconds;
-
-		previous_seconds = current_seconds;
-		
-		 sprintf (tmp, "opengl @ fps: %.2f", fps);
-		 glfwSetWindowTitle (window, tmp);
-		 frame_count = 0;
-	}
-	frame_count++;
+  static double previous_seconds = glfwGetTime ();
+  static int frame_count;
+  double current_seconds = glfwGetTime ();
+  double elapsed_seconds = current_seconds - previous_seconds;
+  if (elapsed_seconds > 0.25) {
+    previous_seconds = current_seconds;
+    double fps = (double)frame_count / elapsed_seconds;
+    char tmp[128];
+    sprintf (tmp, "opengl @ fps: %.2f", fps);
+    glfwSetWindowTitle (window, tmp);
+    frame_count = 0;
+  }
+  frame_count++;
 }
 
 int main () {
