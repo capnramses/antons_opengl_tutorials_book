749e7b8dfd6236b5a4d367a087fedf7614c27c9f
capnramses <lists@antongerdelan.net>
2014-08-11 15:14:29 +0100

    fixed fps function

--- 01_extended_init/main.c
+++ 01_extended_init/main.c
@@ -158,19 +158,16 @@ int frame_count;
 void _update_fps_counter (GLFWwindow* window) {
 	double current_seconds;
 	double elapsed_seconds;
-
-	previous_seconds = glfwGetTime ();
+	
 	current_seconds = glfwGetTime ();
 	elapsed_seconds = current_seconds - previous_seconds;
 	if (elapsed_seconds > 0.25) {
+		previous_seconds = current_seconds;
 		char tmp[128];
 		double fps = (double)frame_count / elapsed_seconds;
-
-		previous_seconds = current_seconds;
-		
-		 sprintf (tmp, "opengl @ fps: %.2f", fps);
-		 glfwSetWindowTitle (window, tmp);
-		 frame_count = 0;
+		sprintf (tmp, "opengl @ fps: %.2f", fps);
+		glfwSetWindowTitle (window, tmp);
+		frame_count = 0;
 	}
 	frame_count++;
 }
