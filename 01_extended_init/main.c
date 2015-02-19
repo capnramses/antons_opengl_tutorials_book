d7ff51f6addb154d2fce79b650ed9785f61d8cda
Aidan Delaney <aidan@ontologyengineering.org>
2015-02-19 09:08:31 +0000

    Ported tutorial 01 to SDL2

--- 01_extended_init/main.c
+++ 01_extended_init/main.c
@@ -1,15 +1,17 @@
 /******************************************************************************\
-| OpenGL 4 Example Code.                                                       |
-| Accompanies written series "Anton's OpenGL 4 Tutorials"                      |
-| Email: anton at antongerdelan dot net                                        |
-| First version 27 Jan 2014                                                    |
-| Copyright Dr Anton Gerdelan, Trinity College Dublin, Ireland.                |
-| See individual libraries for separate legal notices                          |
+| OpenGL 4 Example Code.													   |
+| Accompanies written series "Anton's OpenGL 4 Tutorials"					   |
+| Email: anton at antongerdelan dot net										   |
+| First version 27 Jan 2014													   |
+| Copyright Dr Anton Gerdelan, Trinity College Dublin, Ireland.				   |
+| See individual libraries for separate legal notices						   |
+| SDL2 port: Dr Aidan Delaney <aidan@ontologyengineering.org>				   |
+|			 (c) 2015														   |
 |******************************************************************************|
-| Extended Initialisation. Some extra detail.                                  |
+| Extended Initialisation. Some extra detail.								   |
 \******************************************************************************/
 #include <GL/glew.h> // include GLEW and new version of GL on Windows
-#include <GLFW/glfw3.h> // GLFW helper library
+#include <SDL2/SDL.h> // SDL helper library
 #include <stdio.h>
 #include <time.h>
 #include <assert.h>
@@ -19,6 +21,82 @@
 #define true 1
 #define false 0
 #define GL_LOG_FILE "gl.log"
+
+/*
+ * SDL timers run in separate threads.	In the timer thread
+ * push an event onto the event queue.	This event signifies
+ * to call draw a frame from the thread in which the OpenGL
+ * context was created.
+ */
+Uint32 tick(Uint32 interval, void *param) {
+	SDL_Event event;
+	event.type = SDL_USEREVENT;
+	event.user.code = 0;
+	event.user.data1 = 0;
+	event.user.data2 = 0;
+	SDL_PushEvent(&event);
+	return interval;
+}
+
+SDL_Window* init_scene() {
+	Uint32 width = 640;
+	Uint32 height = 480;
+	SDL_Window * window;
+	SDL_GLContext glContext;
+
+	/* Glew will later ensure that OpenGL 3 *is* supported on this machine */
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+
+	/* Do double buffering in GL */
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+
+  /* Initialise SDL - when using C/C++ it's common to have to
+	 initialise libraries by calling a function within them. */
+	if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_TIMER)<0) {
+		  fprintf(stderr, "Failed to initialise SDL: %s\n", SDL_GetError());
+		  exit(1);
+	}
+
+	/* When we close a window quit the SDL application */
+	atexit(SDL_Quit);
+
+	/* Create a new window with an OpenGL surface */
+	window = SDL_CreateWindow("Shader Example"
+							  , SDL_WINDOWPOS_CENTERED
+							  , SDL_WINDOWPOS_CENTERED
+							  , width
+							  , height
+							  , SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+	if (!window) {
+		fprintf(stderr, "Failed to create SDL window: %s\n", SDL_GetError());
+		exit(1);
+	}
+
+	glContext = SDL_GL_CreateContext(window);
+	if (!glContext) {
+		fprintf(stderr, "Failed to create OpenGL context: %s\n", SDL_GetError());
+		exit(1);
+	}
+
+	/* Initialise GLEW - an easy way to ensure OpenGl 3.0+
+	   The *must* be done after we have set the video mode - otherwise we have no
+	   OpenGL context to test. */
+	glewExperimental = GL_TRUE;
+	glewInit();
+	if (!glewIsSupported("GL_VERSION_3_0")) {
+		fprintf(stderr, "OpenGL 3.0 not available");
+		exit(1);
+	}
+
+	/* tell GL to only draw onto a pixel if the shape is closer to the viewer */
+	glEnable (GL_DEPTH_TEST); /* enable depth-testing */
+	glDepthFunc (GL_LESS);/*depth-testing interprets a smaller value as "closer"*/
+
+	return window;
+}
 
 /* start a new log file. put the time and date at the top */
 bool restart_gl_log () {
@@ -83,9 +161,9 @@ bool gl_log_err (const char* message, ...) {
 	return true;
 }
 
-/* we will tell GLFW to run this function whenever it finds an error */
-void glfw_error_callback (int error, const char* description) {
-	gl_log_err ("GLFW ERROR: code %i msg: %s\n", error, description);
+/* we will tell SDL to run this function whenever it finds an error */
+void sdl_error_callback (int error, const char* description) {
+	gl_log_err ("SDL ERROR: code %i msg: %s\n", error, description);
 }
 
 // keep track of window size for things like the viewport and the mouse cursor
@@ -93,9 +171,10 @@ int g_gl_width = 640;
 int g_gl_height = 480;
 
 /* we will tell GLFW to run this function whenever the window is resized */
-void glfw_window_size_callback (GLFWwindow* window, int width, int height) {
+void sdl_window_size_callback (SDL_Window* window, int width, int height) {
 	g_gl_width = width;
 	g_gl_height = height;
+    SDL_SetWindowSize(window, width, height);
 	printf ("width %i height %i\n", width, height);
 	/* update any perspective matrices used here */
 }
@@ -155,25 +234,26 @@ double previous_seconds;
 int frame_count;
 
 /* we will use this function to update the window title with a frame rate */
-void _update_fps_counter (GLFWwindow* window) {
+void _update_fps_counter (SDL_Window* window) {
 	double current_seconds;
 	double elapsed_seconds;
-	
-	current_seconds = glfwGetTime ();
+
+	current_seconds = SDL_GetTicks () / 1000; //SDL_GetTicks in millis
 	elapsed_seconds = current_seconds - previous_seconds;
 	if (elapsed_seconds > 0.25) {
 		previous_seconds = current_seconds;
 		char tmp[128];
 		double fps = (double)frame_count / elapsed_seconds;
 		sprintf (tmp, "opengl @ fps: %.2f", fps);
-		glfwSetWindowTitle (window, tmp);
+		SDL_SetWindowTitle (window, tmp);
 		frame_count = 0;
 	}
 	frame_count++;
 }
 
 int main () {
-	GLFWwindow* window;
+	SDL_Window* window;
+	SDL_Event event;
 	const GLubyte* renderer;
 	const GLubyte* version;
 	GLfloat points[] = {
@@ -184,14 +264,14 @@ int main () {
 	GLuint vbo;
 	GLuint vao;
 	const char* vertex_shader =
-	"#version 410\n"
+	"#version 130\n"
 	"in vec3 vp;"
 	"void main () {"
 	"	gl_Position = vec4 (vp, 1.0);"
 	"}";
 	
 	const char* fragment_shader =
-	"#version 410\n"
+	"#version 130\n"
 	"out vec4 frag_colour;"
 	"void main () {"
 	"	frag_colour = vec4 (0.5, 0.0, 0.5, 1.0);"
@@ -200,43 +280,11 @@ int main () {
 
 	assert (restart_gl_log ());
 	// start GL context and O/S window using the GLFW helper library
-	gl_log ("starting GLFW\n%s\n", glfwGetVersionString ());
-	// register the error call-back function that we wrote, above
-	glfwSetErrorCallback (glfw_error_callback);
-	if (!glfwInit ()) {
-		fprintf (stderr, "ERROR: could not start GLFW3\n");
-		return 1;
-	}
+	SDL_version linked;
+	SDL_GetVersion(&linked);
+	gl_log ("starting SDL\n%d.%d.%d.\n", linked.major, linked.minor, linked.patch);
 
-	// uncomment these lines if on Apple OS X
-	/*glfwWindowHint (GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint (GLFW_CONTEXT_VERSION_MINOR, 2);
-	glfwWindowHint (GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-	glfwWindowHint (GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-*/
-	/* we can run a full-screen window here */
-	
-	/*GLFWmonitor* mon = glfwGetPrimaryMonitor ();
-	const GLFWvidmode* vmode = glfwGetVideoMode (mon);
-	GLFWwindow* window = glfwCreateWindow (
-		vmode->width, vmode->height, "Extended GL Init", mon, NULL
-	);*/
-
-	window = glfwCreateWindow (
-		g_gl_width, g_gl_height, "Extended Init.", NULL, NULL
-	);
-	if (!window) {
-		fprintf (stderr, "ERROR: could not open window with GLFW3\n");
-		glfwTerminate();
-		return 1;
-	}
-	glfwSetWindowSizeCallback (window, glfw_window_size_callback);
-	glfwMakeContextCurrent (window);
-	
-	glfwWindowHint (GLFW_SAMPLES, 4);
-	// start GLEW extension handler
-	glewExperimental = GL_TRUE;
-	glewInit ();
+	window = init_scene();
 
 	// get version info
 	renderer = glGetString (GL_RENDERER); // get renderer string
@@ -269,27 +317,41 @@ int main () {
 	glAttachShader (shader_programme, fs);
 	glAttachShader (shader_programme, vs);
 	glLinkProgram (shader_programme);
-	
-	while (!glfwWindowShouldClose (window)) {
-		_update_fps_counter (window);
-		// wipe the drawing surface clear
-		glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		glViewport (0, 0, g_gl_width, g_gl_height);
-		
-		glUseProgram (shader_programme);
-		glBindVertexArray (vao);
-		// draw points 0-3 from the currently bound VAO with current in-use shader
-		glDrawArrays (GL_TRIANGLES, 0, 3);
-		// update other events like input handling 
-		glfwPollEvents ();
-		if (GLFW_PRESS == glfwGetKey (window, GLFW_KEY_ESCAPE)) {
-			glfwSetWindowShouldClose (window, 1);
+
+		/* Call the function "tick" every 1/60th second */
+	SDL_AddTimer(1000/60, tick, NULL);
+
+	/* The main event loop */
+	while (SDL_WaitEvent(&event)) {
+		switch (event.type) {
+		case SDL_QUIT:
+			SDL_Quit();
+			break;
+		case SDL_USEREVENT:
+			_update_fps_counter (window);
+			// wipe the drawing surface clear
+			glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+			glViewport (0, 0, g_gl_width, g_gl_height);
+
+			glUseProgram (shader_programme);
+			glBindVertexArray (vao);
+			// draw points 0-3 from the currently bound VAO with current in-use shader
+			glDrawArrays (GL_TRIANGLES, 0, 3);
+
+			// put the stuff we've been drawing onto the display
+			SDL_GL_SwapWindow(window);
+			break;
+        case SDL_WINDOWEVENT:
+          switch (event.window.event) {
+                case SDL_WINDOWEVENT_RESIZED:
+                  sdl_window_size_callback(window, event.window.data1, event.window.data2);
+                break;
+            default:
+                break;
+        }
+		defult:
+			break;
 		}
-		// put the stuff we've been drawing onto the display
-		glfwSwapBuffers (window);
 	}
-	
-	// close GL context and any other GLFW resources
-	glfwTerminate();
 	return 0;
 }
