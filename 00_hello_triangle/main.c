ab2ddb7a10c8693df4a1c6998d451a2d90ca3090
Anton Gerdelan <capnramses@users.noreply.github.com>
2016-08-26 10:18:44 +0100

    tidied/clarified comments

--- 00_hello_triangle/main.c
+++ 00_hello_triangle/main.c
@@ -7,14 +7,10 @@
 | See individual libraries for separate legal notices                          |
 |******************************************************************************|
 | "Hello Triangle". Just the basics.                                           |
-| If you're on Mac un-comment the version number code at the beginning. It     |
+| If you're on Apple un-comment the version number code at the beginning. It   |
 | will give you the latest, even if you say 3.2!                               |
 | This uses the libraries GLEW and GLFW3 to start GL. Download and compile     |
 | these first. Linking them might be a pain, but you'll need to master this.   |
-|                                                                              |
-| I wrote this so that it compiles in pedantic ISO C90, to show that it's      |
-| easily done. I usually use minimalist C++ though, for tidier-looking maths   |
-| functions.                                                                   |
 \******************************************************************************/
 #include <GL/glew.h> /* include GLEW and new version of GL on Windows */
 #include <GLFW/glfw3.h> /* GLFW helper library */
@@ -26,8 +22,7 @@ int main () {
 	const GLubyte* version;
 	GLuint vao;
 	GLuint vbo;
-	/* geometry to use. these are 3 xyz points (9 floats total) to make a
-     triangle */
+	/* geometry to use. these are 3 xyz points (9 floats total) to make a triangle */
 	GLfloat points[] = {
 		 0.0f,	0.5f,	0.0f,
 		 0.5f, -0.5f,	0.0f,
@@ -50,7 +45,7 @@ int main () {
 	"	frag_colour = vec4 (0.5, 0.0, 0.5, 1.0);"
 	"}";
 	/* GL shader objects for vertex and fragment shader [components] */
-	GLuint vs, fs;
+	GLuint vert_shader, frag_shader;
 	/* GL shader programme object [combined, to link] */
 	GLuint shader_programme;
 
@@ -87,16 +82,15 @@ int main () {
 	printf ("OpenGL version supported %s\n", version);
 
 	/* tell GL to only draw onto a pixel if the shape is closer to the viewer
-     */
+	than anything already drawn at that pixel */
 	glEnable (GL_DEPTH_TEST); /* enable depth-testing */
-    /* with LESS depth-testing interprets a smaller value as "closer" */
+    	/* with LESS depth-testing interprets a smaller depth value as meaning "closer" */
 	glDepthFunc (GL_LESS);
 	/* a vertex buffer object (VBO) is created here. this stores an array of
-     data on the graphics adapter's memory. in our case - the vertex points */
+	data on the graphics adapter's memory. in our case - the vertex points */
 	glGenBuffers (1, &vbo);
 	glBindBuffer (GL_ARRAY_BUFFER, vbo);
-	glBufferData (GL_ARRAY_BUFFER, 9 * sizeof (GLfloat), points,
-                  GL_STATIC_DRAW);
+	glBufferData (GL_ARRAY_BUFFER, 9 * sizeof (GLfloat), points, GL_STATIC_DRAW);
 	
 	/* the vertex array object (VAO) is a little descriptor that defines which
 	data from vertex buffer objects should be used as input variables to vertex
@@ -104,39 +98,39 @@ int main () {
 	variable' */
 	glGenVertexArrays (1, &vao);
 	glBindVertexArray (vao);
-	glEnableVertexAttribArray (0);
+	glEnableVertexAttribArray (0); // "attribute #0 should be enabled when this vao is bound"
 	glBindBuffer (GL_ARRAY_BUFFER, vbo);
+	// "attribute #0 is created from every 3 variables in the above buffer, of type float (i.e. make me vec3s)"
 	glVertexAttribPointer (0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
 	
 	/* here we copy the shader strings into GL shaders, and compile them. we
-     then create an executable shader 'program' and attach both of the compiled
-     shaders. we link this, which matches the outputs of the vertex shader to
-     the inputs of the fragment shader, etc. and it is then ready to use */
-	vs = glCreateShader (GL_VERTEX_SHADER);
-	glShaderSource (vs, 1, &vertex_shader, NULL);
-	glCompileShader (vs);
-	fs = glCreateShader (GL_FRAGMENT_SHADER);
-	glShaderSource (fs, 1, &fragment_shader, NULL);
-	glCompileShader (fs);
+	then create an executable shader 'program' and attach both of the compiled
+     	shaders. we link this, which matches the outputs of the vertex shader to
+	the inputs of the fragment shader, etc. and it is then ready to use */
+	vert_shader = glCreateShader (GL_VERTEX_SHADER);
+	glShaderSource (vert_shader, 1, &vertex_shader, NULL);
+	glCompileShader (vert_shader);
+	frag_shader = glCreateShader (GL_FRAGMENT_SHADER);
+	glShaderSource (frag_shader, 1, &fragment_shader, NULL);
+	glCompileShader (frag_shader);
 	shader_programme = glCreateProgram ();
-	glAttachShader (shader_programme, fs);
-	glAttachShader (shader_programme, vs);
+	glAttachShader (shader_programme, frag_shader);
+	glAttachShader (shader_programme, vert_shader);
 	glLinkProgram (shader_programme);
 	
 	/* this loop clears the drawing surface, then draws the geometry described
-     by	the VAO onto the drawing surface. we 'poll events' to see if the window
-     was closed, etc. finally, we 'swap the buffers' which displays our drawing
-     surface onto the view area. we use a double-buffering system which means
-     that we have a 'currently displayed' surface, and 'currently being drawn'
-     surface. hence the 'swap' idea. in a single-buffering system we would see
-     stuff being drawn one-after-the-other */
+     	by the VAO onto the drawing surface. we 'poll events' to see if the window
+	was closed, etc. finally, we 'swap the buffers' which displays our drawing
+     	surface onto the view area. we use a double-buffering system which means
+     	that we have a 'currently displayed' surface, and 'currently being drawn'
+     	surface. hence the 'swap' idea. in a single-buffering system we would see
+     	stuff being drawn one-after-the-other */
 	while (!glfwWindowShouldClose (window)) {
 		/* wipe the drawing surface clear */
 		glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glUseProgram (shader_programme);
 		glBindVertexArray (vao);
-		/* draw points 0-3 from the currently bound VAO with current in-use
-         shader */
+		/* draw points 0-3 from the currently bound VAO with current in-use shader */
 		glDrawArrays (GL_TRIANGLES, 0, 3);
 		/* update other events like input handling */
 		glfwPollEvents ();
