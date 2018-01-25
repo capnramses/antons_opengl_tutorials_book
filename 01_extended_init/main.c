766c7cbbdfb80e05e31e525cba3a42a45a6911ea
Anton Gerdelan <capnramses@users.noreply.github.com>
2018-01-25 18:54:16 +0000

    removed excessive tabs

--- 01_extended_init/main.c
+++ 01_extended_init/main.c
@@ -172,16 +172,16 @@ int main() {
 	GLuint vbo;
 	GLuint vao;
 	const char *vertex_shader = "#version 410\n"
-															"in vec3 vp;"
-															"void main () {"
-															"	gl_Position = vec4 (vp, 1.0);"
-															"}";
+		"in vec3 vp;"
+		"void main() {"
+		"  gl_Position = vec4( vp, 1.0 );"
+		"}";
 
 	const char *fragment_shader = "#version 410\n"
-																"out vec4 frag_colour;"
-																"void main () {"
-																"	frag_colour = vec4 (0.5, 0.0, 0.5, 1.0);"
-																"}";
+		"out vec4 frag_colour;"
+		"void main() {"
+		"  frag_colour = vec4( 0.5, 0.0, 0.5, 1.0 );"
+		"}";
 	GLuint shader_programme, vs, fs;
 
 	restart_gl_log();
