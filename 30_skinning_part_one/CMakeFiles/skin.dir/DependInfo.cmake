1efe61f2d969cd182d69ced0faa53e68161def9a
Mikel Losada <mikel.losada@gmail.com>
2019-03-27 21:04:26 +0100

    Add CMakeLists.txt for folders 04-41

---
+++ 30_skinning_part_one/CMakeFiles/skin.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/mikel/opengl/antons_opengl_tutorials_book/30_skinning_part_one/gl_utils.cpp" "/home/mikel/opengl/antons_opengl_tutorials_book/30_skinning_part_one/CMakeFiles/skin.dir/gl_utils.cpp.o"
+  "/home/mikel/opengl/antons_opengl_tutorials_book/30_skinning_part_one/main.cpp" "/home/mikel/opengl/antons_opengl_tutorials_book/30_skinning_part_one/CMakeFiles/skin.dir/main.cpp.o"
+  "/home/mikel/opengl/antons_opengl_tutorials_book/30_skinning_part_one/maths_funcs.cpp" "/home/mikel/opengl/antons_opengl_tutorials_book/30_skinning_part_one/CMakeFiles/skin.dir/maths_funcs.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "../common/include"
+  "/usr/lib/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 30_skinning_part_one/CMakeFiles/skin.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/skin.dir/main.cpp.o"
+  "CMakeFiles/skin.dir/maths_funcs.cpp.o"
+  "CMakeFiles/skin.dir/gl_utils.cpp.o"
+  "skin.pdb"
+  "skin"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/skin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
