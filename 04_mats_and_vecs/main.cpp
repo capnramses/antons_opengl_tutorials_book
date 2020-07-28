0a1cbf1469126f4fbf571d0009b379a55f31c705
Anton Gerdelan <antonofnote@gmail.com>
2020-07-28 23:21:24 +0100

    clang format

--- 04_mats_and_vecs/main.cpp
+++ 04_mats_and_vecs/main.cpp
@@ -10,8 +10,8 @@
 | Note: code discussed in previous tutorials is moved into gl_utils file       |
 | On Apple don't forget to uncomment the version number hint in start_gl()     |
 \******************************************************************************/
-#include "gl_utils.h"		// utility functions discussed in earlier tutorials
-#include <GL/glew.h>		// include GLEW and new version of GL on Windows
+#include "gl_utils.h"   // utility functions discussed in earlier tutorials
+#include <GL/glew.h>    // include GLEW and new version of GL on Windows
 #include <GLFW/glfw3.h> // GLFW helper library
 #include <assert.h>
 #include <math.h>
@@ -23,149 +23,144 @@
 #define GL_LOG_FILE "gl.log"
 
 // keep track of window size for things like the viewport and the mouse cursor
-int g_gl_width = 640;
-int g_gl_height = 480;
-GLFWwindow *g_window = NULL;
+int g_gl_width       = 640;
+int g_gl_height      = 480;
+GLFWwindow* g_window = NULL;
 
 int main() {
-	restart_gl_log();
-	// all the GLFW and GLEW start-up code is moved to here in gl_utils.cpp
-	start_gl();
-	// tell GL to only draw onto a pixel if the shape is closer to the viewer
-	glEnable( GL_DEPTH_TEST ); // enable depth-testing
-	glDepthFunc( GL_LESS );		 // depth-testing interprets a smaller value as "closer"
+  restart_gl_log();
+  // all the GLFW and GLEW start-up code is moved to here in gl_utils.cpp
+  start_gl();
+  // tell GL to only draw onto a pixel if the shape is closer to the viewer
+  glEnable( GL_DEPTH_TEST ); // enable depth-testing
+  glDepthFunc( GL_LESS );    // depth-testing interprets a smaller value as "closer"
 
-	/* OTHER STUFF GOES HERE NEXT */
-	GLfloat points[] = { 0.0f, 0.5f, 0.0f, 0.5f, -0.5f, 0.0f, -0.5f, -0.5f, 0.0f };
+  /* OTHER STUFF GOES HERE NEXT */
+  GLfloat points[] = { 0.0f, 0.5f, 0.0f, 0.5f, -0.5f, 0.0f, -0.5f, -0.5f, 0.0f };
 
-	GLfloat colours[] = { 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f };
+  GLfloat colours[] = { 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f };
 
-	GLuint points_vbo;
-	glGenBuffers( 1, &points_vbo );
-	glBindBuffer( GL_ARRAY_BUFFER, points_vbo );
-	glBufferData( GL_ARRAY_BUFFER, 9 * sizeof( GLfloat ), points, GL_STATIC_DRAW );
+  GLuint points_vbo;
+  glGenBuffers( 1, &points_vbo );
+  glBindBuffer( GL_ARRAY_BUFFER, points_vbo );
+  glBufferData( GL_ARRAY_BUFFER, 9 * sizeof( GLfloat ), points, GL_STATIC_DRAW );
 
-	GLuint colours_vbo;
-	glGenBuffers( 1, &colours_vbo );
-	glBindBuffer( GL_ARRAY_BUFFER, colours_vbo );
-	glBufferData( GL_ARRAY_BUFFER, 9 * sizeof( GLfloat ), colours, GL_STATIC_DRAW );
+  GLuint colours_vbo;
+  glGenBuffers( 1, &colours_vbo );
+  glBindBuffer( GL_ARRAY_BUFFER, colours_vbo );
+  glBufferData( GL_ARRAY_BUFFER, 9 * sizeof( GLfloat ), colours, GL_STATIC_DRAW );
 
-	GLuint vao;
-	glGenVertexArrays( 1, &vao );
-	glBindVertexArray( vao );
-	glBindBuffer( GL_ARRAY_BUFFER, points_vbo );
-	glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 0, NULL );
-	glBindBuffer( GL_ARRAY_BUFFER, colours_vbo );
-	glVertexAttribPointer( 1, 3, GL_FLOAT, GL_FALSE, 0, NULL );
-	glEnableVertexAttribArray( 0 );
-	glEnableVertexAttribArray( 1 );
+  GLuint vao;
+  glGenVertexArrays( 1, &vao );
+  glBindVertexArray( vao );
+  glBindBuffer( GL_ARRAY_BUFFER, points_vbo );
+  glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 0, NULL );
+  glBindBuffer( GL_ARRAY_BUFFER, colours_vbo );
+  glVertexAttribPointer( 1, 3, GL_FLOAT, GL_FALSE, 0, NULL );
+  glEnableVertexAttribArray( 0 );
+  glEnableVertexAttribArray( 1 );
 
-	char vertex_shader[1024 * 256];
-	char fragment_shader[1024 * 256];
-	parse_file_into_str( "test_vs.glsl", vertex_shader, 1024 * 256 );
-	parse_file_into_str( "test_fs.glsl", fragment_shader, 1024 * 256 );
+  char vertex_shader[1024 * 256];
+  char fragment_shader[1024 * 256];
+  parse_file_into_str( "test_vs.glsl", vertex_shader, 1024 * 256 );
+  parse_file_into_str( "test_fs.glsl", fragment_shader, 1024 * 256 );
 
-	GLuint vs = glCreateShader( GL_VERTEX_SHADER );
-	const GLchar *p = (const GLchar *)vertex_shader;
-	glShaderSource( vs, 1, &p, NULL );
-	glCompileShader( vs );
+  GLuint vs       = glCreateShader( GL_VERTEX_SHADER );
+  const GLchar* p = (const GLchar*)vertex_shader;
+  glShaderSource( vs, 1, &p, NULL );
+  glCompileShader( vs );
 
-	// check for compile errors
-	int params = -1;
-	glGetShaderiv( vs, GL_COMPILE_STATUS, &params );
-	if ( GL_TRUE != params ) {
-		fprintf( stderr, "ERROR: GL shader index %i did not compile\n", vs );
-		print_shader_info_log( vs );
-		return 1; // or exit or something
-	}
+  // check for compile errors
+  int params = -1;
+  glGetShaderiv( vs, GL_COMPILE_STATUS, &params );
+  if ( GL_TRUE != params ) {
+    fprintf( stderr, "ERROR: GL shader index %i did not compile\n", vs );
+    print_shader_info_log( vs );
+    return 1; // or exit or something
+  }
 
-	GLuint fs = glCreateShader( GL_FRAGMENT_SHADER );
-	p = (const GLchar *)fragment_shader;
-	glShaderSource( fs, 1, &p, NULL );
-	glCompileShader( fs );
+  GLuint fs = glCreateShader( GL_FRAGMENT_SHADER );
+  p         = (const GLchar*)fragment_shader;
+  glShaderSource( fs, 1, &p, NULL );
+  glCompileShader( fs );
 
-	// check for compile errors
-	glGetShaderiv( fs, GL_COMPILE_STATUS, &params );
-	if ( GL_TRUE != params ) {
-		fprintf( stderr, "ERROR: GL shader index %i did not compile\n", fs );
-		print_shader_info_log( fs );
-		return 1; // or exit or something
-	}
+  // check for compile errors
+  glGetShaderiv( fs, GL_COMPILE_STATUS, &params );
+  if ( GL_TRUE != params ) {
+    fprintf( stderr, "ERROR: GL shader index %i did not compile\n", fs );
+    print_shader_info_log( fs );
+    return 1; // or exit or something
+  }
 
-	GLuint shader_programme = glCreateProgram();
-	glAttachShader( shader_programme, fs );
-	glAttachShader( shader_programme, vs );
-	glLinkProgram( shader_programme );
+  GLuint shader_programme = glCreateProgram();
+  glAttachShader( shader_programme, fs );
+  glAttachShader( shader_programme, vs );
+  glLinkProgram( shader_programme );
 
-	glGetProgramiv( shader_programme, GL_LINK_STATUS, &params );
-	if ( GL_TRUE != params ) {
-		fprintf( stderr, "ERROR: could not link shader programme GL index %i\n",
-						 shader_programme );
-		print_programme_info_log( shader_programme );
-		return false;
-	}
+  glGetProgramiv( shader_programme, GL_LINK_STATUS, &params );
+  if ( GL_TRUE != params ) {
+    fprintf( stderr, "ERROR: could not link shader programme GL index %i\n", shader_programme );
+    print_programme_info_log( shader_programme );
+    return false;
+  }
 
-	GLfloat matrix[] = {
-		1.0f, 0.0f, 0.0f, 0.0f, // first column
-		0.0f, 1.0f, 0.0f, 0.0f, // second column
-		0.0f, 0.0f, 1.0f, 0.0f, // third column
-		0.5f, 0.0f, 0.0f, 1.0f	// fourth column
-	};
+  GLfloat matrix[] = {
+    1.0f, 0.0f, 0.0f, 0.0f, // first column
+    0.0f, 1.0f, 0.0f, 0.0f, // second column
+    0.0f, 0.0f, 1.0f, 0.0f, // third column
+    0.5f, 0.0f, 0.0f, 1.0f  // fourth column
+  };
 
-	int matrix_location = glGetUniformLocation( shader_programme, "matrix" );
-	glUseProgram( shader_programme );
-	glUniformMatrix4fv( matrix_location, 1, GL_FALSE, matrix );
+  int matrix_location = glGetUniformLocation( shader_programme, "matrix" );
+  glUseProgram( shader_programme );
+  glUniformMatrix4fv( matrix_location, 1, GL_FALSE, matrix );
 
-	glEnable( GL_CULL_FACE ); // cull face
-	glCullFace( GL_BACK );		// cull back face
-	glFrontFace( GL_CW );			// GL_CCW for counter clock-wise
+  glEnable( GL_CULL_FACE ); // cull face
+  glCullFace( GL_BACK );    // cull back face
+  glFrontFace( GL_CW );     // GL_CCW for counter clock-wise
 
-	float speed = 1.0f; // move at 1 unit per second
-	float last_position = 0.0f;
-	while ( !glfwWindowShouldClose( g_window ) ) {
-		// add a timer for doing animation
-		static double previous_seconds = glfwGetTime();
-		double current_seconds = glfwGetTime();
-		double elapsed_seconds = current_seconds - previous_seconds;
-		previous_seconds = current_seconds;
+  float speed         = 1.0f; // move at 1 unit per second
+  float last_position = 0.0f;
+  while ( !glfwWindowShouldClose( g_window ) ) {
+    // add a timer for doing animation
+    static double previous_seconds = glfwGetTime();
+    double current_seconds         = glfwGetTime();
+    double elapsed_seconds         = current_seconds - previous_seconds;
+    previous_seconds               = current_seconds;
 
-		_update_fps_counter( g_window );
-		// wipe the drawing surface clear
-		glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
-		glViewport( 0, 0, g_gl_width, g_gl_height );
+    _update_fps_counter( g_window );
+    // wipe the drawing surface clear
+    glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+    glViewport( 0, 0, g_gl_width, g_gl_height );
 
-		//
-		// Note: this call is not necessary, but I like to do it anyway before any
-		// time that I call glDrawArrays() so I never use the wrong shader programme
-		glUseProgram( shader_programme );
+    //
+    // Note: this call is not necessary, but I like to do it anyway before any
+    // time that I call glDrawArrays() so I never use the wrong shader programme
+    glUseProgram( shader_programme );
 
-		// update the matrix
-		// - you could simplify this by just using sin(current_seconds)
-		matrix[12] = elapsed_seconds * speed + last_position;
-		last_position = matrix[12];
-		if ( fabs( last_position ) > 1.0 ) {
-			speed = -speed;
-		}
-		//
-		// Note: this call is related to the most recently 'used' shader programme
-		glUniformMatrix4fv( matrix_location, 1, GL_FALSE, matrix );
+    // update the matrix
+    // - you could simplify this by just using sin(current_seconds)
+    matrix[12]    = elapsed_seconds * speed + last_position;
+    last_position = matrix[12];
+    if ( fabs( last_position ) > 1.0 ) { speed = -speed; }
+    //
+    // Note: this call is related to the most recently 'used' shader programme
+    glUniformMatrix4fv( matrix_location, 1, GL_FALSE, matrix );
 
-		//
-		// Note: this call is not necessary, but I like to do it anyway before any
-		// time that I call glDrawArrays() so I never use the wrong vertex data
-		glBindVertexArray( vao );
-		// draw points 0-3 from the currently bound VAO with current in-use shader
-		glDrawArrays( GL_TRIANGLES, 0, 3 );
-		// update other events like input handling
-		glfwPollEvents();
-		if ( GLFW_PRESS == glfwGetKey( g_window, GLFW_KEY_ESCAPE ) ) {
-			glfwSetWindowShouldClose( g_window, 1 );
-		}
-		// put the stuff we've been drawing onto the display
-		glfwSwapBuffers( g_window );
-	}
+    //
+    // Note: this call is not necessary, but I like to do it anyway before any
+    // time that I call glDrawArrays() so I never use the wrong vertex data
+    glBindVertexArray( vao );
+    // draw points 0-3 from the currently bound VAO with current in-use shader
+    glDrawArrays( GL_TRIANGLES, 0, 3 );
+    // update other events like input handling
+    glfwPollEvents();
+    if ( GLFW_PRESS == glfwGetKey( g_window, GLFW_KEY_ESCAPE ) ) { glfwSetWindowShouldClose( g_window, 1 ); }
+    // put the stuff we've been drawing onto the display
+    glfwSwapBuffers( g_window );
+  }
 
-	// close GL context and any other GLFW resources
-	glfwTerminate();
-	return 0;
+  // close GL context and any other GLFW resources
+  glfwTerminate();
+  return 0;
 }
