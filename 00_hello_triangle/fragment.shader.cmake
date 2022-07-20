f94a4d3c13f21ce38f907bd75112015c37ea267a
Yuri Kuznetsov <yyk@plein-2>
2022-07-20 06:14:51 -0400

    Added main_exp1.c

---
+++ 00_hello_triangle/fragment.shader.cmake
@@ -0,0 +1,5 @@
+#version 410
+out vec4 frag_colour;
+void main () {
+    frag_colour = vec4(0.5, 0.0, 0.0, 1.0);
+};
---
+++ 00_hello_triangle/vertex.shader.cmake
@@ -0,0 +1,5 @@
+#version 410
+in vec3 vp;
+void main () {
+  gl_Position = vec4(vp, 1.0);
+};
