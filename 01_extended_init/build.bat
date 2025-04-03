967d47d195cc6d381c5fa1921ac294c688e6dbe3
Anton Gerdelan <capnramses@users.noreply.github.com>
2025-04-03 19:49:44 +0100

    Update build.bat

--- 01_extended_init/build.bat
+++ 01_extended_init/build.bat
@@ -22,15 +22,13 @@ call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build
 :setpaths
 
 set LFLAGS=/DEBUG /MACHINE:X64
-set INCLUDES=/I "..\third_party\glew-2.1.0\include" /I "..\third_party\glfw-3.4.bin.WIN64\include" /I "..\third_party\assimp\include"
+set INCLUDES=/I "..\third_party\glew-2.1.0\include" /I "..\third_party\glfw-3.4.bin.WIN64\include"
 set LIB_PATH_GLFW=/LIBPATH:"..\third_party\glfw-3.4.bin.WIN64\lib-vc2022"
 set LIB_PATH_GLEW=/LIBPATH:"..\third_party\glew-2.1.0\lib\Release\x64"
-set LIB_PATH_ASSIMP=/LIBPATH:"..\third_party\assimp\lib\"
 set SYSTEM_LIBS="kernel32.lib" "user32.lib" "gdi32.lib" "winspool.lib" "comdlg32.lib" "advapi32.lib" "shell32.lib" "ole32.lib" "oleaut32.lib" "uuid.lib" "odbc32.lib" "odbccp32.lib"
 set LIBS=%LIB_PATH_GLFW% %LIB_PATH_GLEW% glew32.lib glfw3dll.lib OpenGL32.lib %SYSTEM_LIBS%
 set DLL_PATH_GLEW="third_party\glew-2.1.0\bin\Release\x64\glew32.dll"
 set DLL_PATH_GLFW="third_party\glfw-3.4.bin.WIN64\lib-vc2019\glfw3.dll"
-set DLL_PATH_ASSIMP="third_party\assimp\bin\vs2022\assimp-vc143-mt.dll
 set SRC="*.c??"
 
 @echo on
